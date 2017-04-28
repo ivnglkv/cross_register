ac2b99d8803a87f5eb683c4d14ff4fccf1f2196a
Golikov Ivan <ivnglkv@eml.cc>
2017-04-28 15:16:50 +0300

    Добавление моделей в админку

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Building,
+    Cabinet,
+    Extension,
+    Location,
+    PBX,
+    PBXPort,
+    Phone,
+    Room,
+    Subscriber,
+    Trunk,
+)
+
+
+admin.site.register(
+    [Building,
+     Cabinet,
+     Extension,
+     Location,
+     PBX,
+     PBXPort,
+     Phone,
+     Room,
+     Subscriber,
+     Trunk,
+     ]
+)
--- journal/apps.py
+++ journal/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class JournalConfig(AppConfig):
     name = 'journal'
+    verbose_name = 'журнал'
