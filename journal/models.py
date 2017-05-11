5148ea9acccfde88240c36f4fe3cc29cbdcf7184
Golikov Ivan <ivnglkv@eml.cc>
2017-05-11 14:01:57 +0300

    Получение кода sql из файла

--- journal/models.py
+++ journal/models.py
@@ -204,21 +204,18 @@ class Phone(CrossPoint):
         src = None
 
         with connection.cursor() as cursor:
-            cursor.execute('''
-                WITH RECURSIVE
-                Rec (id, destination_id, location_id)
-                AS (
-                      SELECT *, id AS main_id
-                        FROM journal_crosspoint
-                       WHERE destination_id IS NULL
-                    UNION ALL
-                      SELECT cp.*, r.main_id AS main_id
-                        FROM Rec r
-                        JOIN journal_crosspoint cp ON (r.id = cp.destination_id)
-                )
-                SELECT main_id
-                  FROM Rec
-                 WHERE id = {}'''.format(self.pk))
+            from os import path
+            from django.conf import settings
+
+            sqlfile = open(path.join(settings.BASE_DIR,
+                                     'journal',
+                                     'sql',
+                                     'get_crosspoint_parent.sql',
+                                     ),
+                           'r')
+            sql = sqlfile.read().replace('/n', ' ')
+
+            cursor.execute(sql.format(self.pk))
 
             row = cursor.fetchone()
             try:
