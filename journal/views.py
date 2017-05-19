2668894893b5d80259b5c52a3e5c4395642e59a7
Golikov Ivan <ivnglkv@eml.cc>
2017-05-19 16:25:40 +0300

    Исправление форматирования комментария

--- journal/views.py
+++ journal/views.py
@@ -17,7 +17,7 @@ class CrosspathPoint:
 
 
 def dictfetchall(cursor):
-    "Return all rows from a cursor as a dict"
+    """Return all rows from a cursor as a dict"""
     columns = [col[0] for col in cursor.description]
     return [
         dict(zip(columns, row))
