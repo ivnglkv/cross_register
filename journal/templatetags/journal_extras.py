43d0e5934bc6b7e27a0282c931b62d1008fffd22
Golikov Ivan <ivnglkv@eml.cc>
2017-05-24 11:29:31 +0300

    Более хорошая поддержка параллелей в табличной форме. Идущие подряд точки объединяются в одну ячейку

--- journal/templatetags/journal_extras.py
+++ journal/templatetags/journal_extras.py
@@ -6,16 +6,16 @@ register = template.Library()
 
 
 @register.simple_tag(name='get_point_path_table')
-def get_point_path_table(point, start_new_line):
+def get_point_path_table(point, start_new_line, cells):
     res = '<tr>' if start_new_line else ''
 
     if point.level > 0 and start_new_line:
-        res += '<td></td>' * (point.level * 2 - 1)
-        res += '<td>↘</td>'
+        res += '<td colspan={}></td>'.format(cells)
+        res += '<td>↘ '
     elif point.level > 0:
-        res += '<td>→</td>'
-
-    res += '<td>'
+        res += ' → '
+    else:
+        res += '<td>'
 
     if point.level > 0:
         res += '<a href="{}">'.format(point.admin_url) + \
@@ -24,13 +24,16 @@ def get_point_path_table(point, start_new_line):
     else:
         res += CrossPoint.objects.get(pk=point.crosspoint_id).journal_str()
 
-    res += '</td>'
+    if len(point.destination) > 1:
+        res += '</td><td>'
+        cells += 1
 
     for line_num, child in enumerate(point.destination):
         new_line = True if line_num > 0 else False
 
-        res += get_point_path_table(child, new_line)
+        res += get_point_path_table(child, new_line, cells)
 
-    res += '</tr>'
+    if not point.destination:
+        res += '</tr>'
 
     return res
