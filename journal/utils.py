4c138d771833845c00c116224ad354e9234b9d48
Golikov Ivan <ivnglkv.eml.cc>
2017-06-18 20:16:54 +0300

    Перенос общих методов и классов из journal.views в journal.utils

---
+++ journal/utils.py
@@ -0,0 +1,113 @@
+from django.db import connection
+from django.urls import reverse
+
+from .models import CrossPoint
+
+
+class CrosspathPoint:
+    crosspoint_id = None
+    location_id = None
+    parent_id = None
+    main_src_id = None
+    level = 0
+    admin_url = ''
+    destination = None
+
+    def __init__(self, crosspoint=None):
+        if crosspoint is not None:
+            self.crosspoint_id = crosspoint['id']
+            self.location_id = crosspoint['location_id']
+            self.parent_id = crosspoint['parent']
+            self.main_src_id = crosspoint['main_src_id']
+            self.level = crosspoint['level']
+
+            cp_tmp = CrossPoint.objects.get(pk=self.crosspoint_id)
+            self.admin_url = reverse(
+                'admin:journal_{}_change'.format(cp_tmp.get_subclass()._meta.model_name),
+                args=(self.crosspoint_id,))
+
+            self.destination = []
+        else:
+            pass
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        res = '{0} crosspoint_id: {1}, ' \
+              'main_src_id: {2}, parent_id: {3}, level: {4}\n' \
+              '{0} destination: {5}'.format('  ' * self.level,
+                                            self.crosspoint_id,
+                                            self.main_src_id,
+                                            self.parent_id,
+                                            self.level,
+                                            self.destination,
+                                            )
+        return res
+
+    def find_parent(self, parent_id):
+        result = None
+
+        for child in self.destination:
+            if child.crosspoint_id == parent_id:
+                result = child
+            else:
+                result = child.find_parent(parent_id)
+
+            if result:
+                break
+
+        return result
+
+
+def dictfetchall(cursor):
+    """Return all rows from a cursor as a dict"""
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+
+def get_crosspath(points):
+    crosspoints = {}
+
+    with connection.cursor() as cursor:
+        from os import path
+        from django.conf import settings
+
+        sqlfile = open(path.join(settings.BASE_DIR,
+                                 'journal',
+                                 'sql',
+                                 'get_crosspath.sql',
+                                 ),
+                       'r')
+        sql = sqlfile.read()
+
+        if len(points) > 0:
+            if len(points) == 1:
+                points_ids_str = '({})'.format(points[0])
+            else:
+                points_ids_str = '{}'.format(points)
+
+            cursor.execute(sql.format(points_ids_str))
+            crosspoints = dictfetchall(cursor)
+
+    crosspath = []
+    current_crosspath_index = -1
+
+    for point in crosspoints:
+        cur_point = CrosspathPoint(point)
+
+        if cur_point.level == 1:
+            crosspath[current_crosspath_index].destination.append(cur_point)
+        elif cur_point.level >= 2:
+            source_point = crosspath[current_crosspath_index]
+            parent = source_point.find_parent(cur_point.parent_id)
+            parent.destination.append(cur_point)
+            pass
+        else:
+            crosspath.append(cur_point)
+            current_crosspath_index += 1
+
+    return crosspath
