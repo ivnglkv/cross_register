3d13ed6db96f13965678daa882f07d32c67cc301
Golikov Ivan <ivnglkv@eml.cc>
2017-06-30 14:55:13 +0300

    Минорные исправления

--- journal/apps.py
+++ journal/apps.py
@@ -1,14 +1,14 @@
 from django.apps import AppConfig
 from django.db.models import signals
 
-import journal.signals as journal_signals
-
 
 class JournalConfig(AppConfig):
     name = 'journal'
     verbose_name = 'Журнал'
 
     def ready(self):
+        import journal.signals as journal_signals
+
         from .models import CrossPoint, PBXPort, PunchBlock, Phone
 
         signals.post_save.connect(journal_signals.pbxport_post_save, sender=PBXPort)
