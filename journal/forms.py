49f3dc2f4f1c34e1a1e7ba07590292aac8751a13
Golikov Ivan <ivnglkv@eml.cc>
2017-07-11 13:28:30 +0300

    Установка chosen полей в старых формах

--- journal/forms.py
+++ journal/forms.py
@@ -4,49 +4,79 @@ Author: Golikov Ivan
 Date: 10.07.2017
 """
 
-from django.forms import ModelForm, ModelChoiceField
+from django.forms import ModelForm
 from django.db.models import Q
 
-from .fields import CrosspointField
+from .fields import (
+    ChosenField,
+    CrosspointField,
+    ModelChosenField,
+)
 from .models import (
     Location,
     PBX,
     PBXPort,
     Phone,
     PunchBlock,
+    PunchBlockType,
 )
 
 
 class PunchBlockForm(ModelForm):
-    location = ModelChoiceField(label='Расположение',
-                                queryset=Location.objects.filter(
-                                    Q(cabinet__isnull=False) |
-                                    Q(room__pbxroom__isnull=False)),
-                                )
+    type = ModelChosenField(
+        label='Тип',
+        queryset=PunchBlockType.objects.all(),
+    )
+    location = ModelChosenField(
+        label='Расположение',
+        queryset=Location.objects.filter(
+            Q(cabinet__isnull=False) |
+            Q(room__pbxroom__isnull=False)),
+    )
     source = CrosspointField(label='Откуда приходит',
                              required=False,
                              )
 
     class Meta:
         model = PunchBlock
-        fields = ['type', 'location', 'source', 'number', 'is_station']
+        fields = [
+            'type',
+            'location',
+            'source',
+            'number',
+            'is_station',
+        ]
 
 
 class PhoneForm(ModelForm):
-    location = ModelChoiceField(label='Расположение',
-                                queryset=Location.objects.filter(
-                                    Q(room__isnull=False))
-                                )
+    location = ModelChosenField(
+        label='Расположение',
+        queryset=Location.objects.filter(
+            Q(room__isnull=False)),
+    )
     source = CrosspointField(label='Откуда приходит',
                              required=False,
                              )
 
     class Meta:
         model = Phone
-        fields = ['location', 'source']
+        fields = [
+            'location',
+            'source',
+        ]
 
 
 class PBXPortForm(ModelForm):
+    pbx = ModelChosenField(
+        label='АТС',
+        queryset=PBX.objects.all(),
+    )
+    type = ChosenField(
+        label='Тип',
+        choices=PBXPort.PORT_TYPES,
+        initial=PBXPort._meta.get_field('type').default,
+    )
+
     class Meta:
         model = PBXPort
         fields = [
@@ -59,11 +89,16 @@ class PBXPortForm(ModelForm):
 
 
 class PBXForm(ModelForm):
-    location = ModelChoiceField(label='Расположение',
-                                queryset=Location.objects.filter(
-                                    Q(cabinet__isnull=False) |
-                                    Q(room__pbxroom__isnull=False)),
-                                )
+    manufacturer = ChosenField(
+        label='Производитель',
+        choices=PBX.MANUFACTURERS,
+    )
+    location = ModelChosenField(
+        label='Расположение',
+        queryset=Location.objects.filter(
+            Q(cabinet__isnull=False) |
+            Q(room__pbxroom__isnull=False)),
+    )
 
     class Meta:
         model = PBX
