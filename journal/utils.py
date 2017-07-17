962af1da294001796734a9c3f62d61dc7f6cdfa2
Golikov Ivan <ivnglkv@eml.cc>
2017-07-17 23:00:26 +0300

    Удалена функция dictfetchall

--- journal/utils.py
+++ journal/utils.py
@@ -87,15 +87,6 @@ class CrosspathPoint:
         return result
 
 
-def dictfetchall(cursor):
-    """Return all rows from a cursor as a dict"""
-    columns = [col[0] for col in cursor.description]
-    return [
-        dict(zip(columns, row))
-        for row in cursor.fetchall()
-    ]
-
-
 def get_crosspath(source):
     """
     source -- int или tuple
