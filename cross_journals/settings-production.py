1d89b69028eba3aa1daeb2d2b2c35b9ff3849de7
Golikov Ivan <ivnglkv@eml.cc>
2017-07-19 13:22:14 +0300

    Добавление приложения polymorphic в INSTALLED_APPS

--- cross_journals/settings-production.py
+++ cross_journals/settings-production.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'journal.apps.JournalConfig',
     'common.apps.CommonConfig',
+    'polymorphic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
