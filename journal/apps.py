153330fc6110a28519e1fc63f1f88ff501bcdb89
Golikov Ivan <ivnglkv@eml.cc>
2017-07-24 01:11:25 +0300

    Рефакторинг соединения с сигналами, прослушка сигналов КРТ

--- journal/apps.py
+++ journal/apps.py
@@ -1,11 +1,17 @@
 """
 Release: 0.2.2
 Author: Golikov Ivan
-Date: 19.07.2017
+Date: 23.07.2017
 """
 
 from django.apps import AppConfig
-from django.db.models import signals
+from django.db.models.signals import (
+    m2m_changed,
+    post_delete,
+    post_save,
+    pre_delete,
+    pre_save,
+)
 
 
 class JournalConfig(AppConfig):
@@ -13,29 +19,75 @@ class JournalConfig(AppConfig):
     verbose_name = 'Журнал'
 
     def ready(self):
-        import journal.signals as journal_signals
+        from journal.signals import (
+            autocreate_location,
+            on_crosspoint_post_change,
+            on_crosspoint_pre_change,
+            pbxport_post_save,
+            subscriber_phones_changed,
+            subscriber_post_changed,
+            subscriber_pre_changed,
+        )
 
-        from .models import Cabinet, CrossPoint, PBXPort, PunchBlock, Phone, Room, Subscriber
+        from .models import (
+            Cabinet,
+            CrossPoint,
+            ExtensionBox,
+            PBXPort,
+            Phone,
+            PunchBlock,
+            Room,
+            Subscriber,
+        )
 
-        signals.post_save.connect(journal_signals.pbxport_post_save, sender=PBXPort)
+        post_save.connect(pbxport_post_save, sender=PBXPort)
 
-        signals.pre_save.connect(journal_signals.on_crosspoint_pre_change, sender=PunchBlock)
-        signals.pre_save.connect(journal_signals.on_crosspoint_pre_change, sender=Phone)
-        signals.pre_delete.connect(journal_signals.on_crosspoint_pre_change, sender=PunchBlock)
-        signals.pre_delete.connect(journal_signals.on_crosspoint_pre_change, sender=Phone)
+        crosspoints_classes = [
+            ExtensionBox,
+            Phone,
+            PunchBlock,
+        ]
 
-        signals.post_save.connect(journal_signals.on_crosspoint_post_change, sender=PunchBlock)
-        signals.post_save.connect(journal_signals.on_crosspoint_post_change, sender=Phone)
+        # По неизвестной мне причине, метод append в данном месте возвращает None
+        # В консоли всё хорошо
+        ext_crosspoints_classes = crosspoints_classes + [CrossPoint]
 
-        signals.post_save.connect(journal_signals.on_crosspoint_post_change, sender=CrossPoint)
+        locations_classes = [
+            Cabinet,
+            Room,
+        ]
 
-        signals.post_delete.connect(journal_signals.on_crosspoint_post_change, sender=CrossPoint)
+        # Сигналы от точек кросса
+        multiple_connect(pre_save, on_crosspoint_pre_change, crosspoints_classes)
+        print(crosspoints_classes)
+        print(CrossPoint)
+        print(crosspoints_classes.append(CrossPoint))
+        multiple_connect(post_save, on_crosspoint_post_change, ext_crosspoints_classes)
+        multiple_connect(pre_delete, on_crosspoint_post_change, crosspoints_classes)
+        post_delete.connect(on_crosspoint_post_change, sender=CrossPoint)
 
-        signals.post_save.connect(journal_signals.autocreate_location, sender=Cabinet)
-        signals.post_save.connect(journal_signals.autocreate_location, sender=Room)
+        # Сигналы от расположений
+        multiple_connect(post_save, autocreate_location, locations_classes)
 
-        signals.m2m_changed.connect(journal_signals.subscriber_phones_changed, sender=Subscriber.phones.through)
+        # Сигналы от абонента
+        pre_save.connect(subscriber_pre_changed, sender=Subscriber)
+        post_save.connect(subscriber_post_changed, sender=Subscriber)
+        post_delete.connect(subscriber_post_changed, sender=Subscriber)
+        m2m_changed.connect(subscriber_phones_changed, sender=Subscriber.phones.through)
 
-        signals.pre_save.connect(journal_signals.subscriber_pre_changed, sender=Subscriber)
-        signals.post_save.connect(journal_signals.subscriber_post_changed, sender=Subscriber)
-        signals.post_delete.connect(journal_signals.subscriber_post_changed, sender=Subscriber)
+
+def multiple_connect(signal, handler, senders):
+    """Соединение одного сигнала от нескольких отправителей с одним обработчиком
+
+    Parameters
+    ----------
+    signal : Signal
+        Сигнал, к которому нужно присоединить получателей
+    handler : func
+        Функция-обработчик сигнала
+    senders : iterable
+        Список отправителей
+    """
+
+    for sender in senders:
+        signal.connect(handler, sender=sender)
